dc86af7c791d3aa4c07dca0f26dcf2194b9aa6fa
Rodrigo261 <102800116+Rodrigo261@users.noreply.github.com>
2022-06-07 22:41:26 +0100

    criaçao de layout,e criaçao das tabela de bd

--- app/src/main/java/com/myapplication/categoria.kt
+++ app/src/main/java/com/myapplication/categoria.kt
@@ -3,7 +3,7 @@ package com.example.myapplication
 import android.content.ContentValues
 import android.provider.BaseColumns
 
- data class categoria (var nome: String, var id: Long = -1){
+ data class categorias (var nome: String, var id: Long = -1){
 
         fun toContentValues() : ContentValues {
             val valores = ContentValues()
