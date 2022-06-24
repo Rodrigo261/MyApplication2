f64f1538188f4117b3200a2b9a4eb404d683e2dd
Rodrigo261 <102800116+Rodrigo261@users.noreply.github.com>
2022-06-24 10:10:46 +0100

    alteraçao na tabela clientes e eliminaçao da funçao bd

--- app/src/main/java/com/myapplication/Clientes.kt
+++ app/src/main/java/com/myapplication/Clientes.kt
@@ -3,7 +3,6 @@ package com.myapplication
 import android.content.ContentValues
 import android.database.Cursor
 import android.provider.BaseColumns
-import com.example.myapplication.TabelaBDClientes
 
 data class Clientes(
     var nomeCliente : String,
@@ -14,8 +13,8 @@ data class Clientes(
     fun toContentValues() : ContentValues {
         val valores = ContentValues()
 
-        valores.put(TabelaBDClientes.CAMPO_Nome, nomeCliente)
-        valores.put(TabelaBDClientes.CAMPO_Saldo, saldo)
+        valores.put(TabelaBDClientes.CAMPO_NOME, nomeCliente)
+        valores.put(TabelaBDClientes.CAMPO_SALDO, saldo)
 
 
 
@@ -25,8 +24,8 @@ data class Clientes(
     companion object {
         fun fromCursor(cursor: Cursor): Clientes {
             val posId = cursor.getColumnIndex(BaseColumns._ID)
-            val posNome = cursor.getColumnIndex(TabelaBDClientes.CAMPO_Nome)
-            val posSaldo = cursor.getColumnIndex(TabelaBDClientes.CAMPO_Saldo)
+            val posNome = cursor.getColumnIndex(TabelaBDClientes.CAMPO_NOME)
+            val posSaldo = cursor.getColumnIndex(TabelaBDClientes.CAMPO_SALDO)
 
             val id = cursor.getLong(posId)
             val nomeCliente = cursor.getString(posNome)
--- app/src/main/java/com/myapplication/tabelaBDClientes.kt
+++ app/src/main/java/com/myapplication/tabelaBDClientes.kt
@@ -1,23 +1,22 @@
-package com.example.myapplication
+package com.myapplication
 
 import android.database.sqlite.SQLiteDatabase
 import android.provider.BaseColumns
-import com.myapplication.TabelaBD
 
 
 class TabelaBDClientes(db: SQLiteDatabase) : TabelaBD(db, NOME) {
     override fun cria() {
         db.execSQL("CREATE TABLE $NOME (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT," +
-                " $CAMPO_Nome TEXT NOT NULL, " +
-                "$CAMPO_Saldo INTEGER NOT NULL)")
+                " $CAMPO_NOME TEXT NOT NULL, " +
+                "$CAMPO_SALDO INTEGER NOT NULL)")
     }
 
     companion object {
         const val NOME = "clientes"
-        const val CAMPO_Nome= "cliente"
-        const val CAMPO_Saldo = "saldo"
+        const val CAMPO_NOME= "cliente"
+        const val CAMPO_SALDO = "saldo"
 
 
-        val TODAS_COLUNAS = arrayOf(BaseColumns._ID, CAMPO_Nome, CAMPO_Saldo)
+        val TODAS_COLUNAS = arrayOf(BaseColumns._ID, CAMPO_NOME, CAMPO_SALDO)
     }
 }
